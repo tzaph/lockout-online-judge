57aee898fd304746e019ad0645bae52233ca1ea1
Maximilliano Utomo <65610949+tzaph@users.noreply.github.com>
2023-06-25 21:46:46 +0700

    Update index.js
    Change netlify link

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://jazzy-speculoos-fa7c28.netlify.app",
+    origin: "https://stellar-cactus-9b498b.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
