3a3e50b14c2ea5ecc454562660443a54c46663d2
Joseph Oliver Lim <81511361+josepholim@users.noreply.github.com>
2023-06-25 22:24:50 +0800

    Update origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://jazzy-speculoos-fa7c28.netlify.app",
     methods: ["GET", "POST"],
   },
 });
